95a59c3acb65ad46fbe267e3615391835db3fcc3
colmultimedia <50298394+colmultimedia@users.noreply.github.com>
2020-08-28 17:10:47 -0500

    ecma8

---
+++ ecma/es7/index.js
@@ -0,0 +1,17 @@
+// ECMA VERSION 7 2016
+
+let numbers = [1,2,3,4,5,6];
+
+if (numbers.includes(5)) {
+	console.log('Si se encuentra')
+}else {
+console.log('No se encuentra el valor');
+}
+
+
+// como elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result = base**exponent; //  usando el doble asterisco
+console.log(result);
